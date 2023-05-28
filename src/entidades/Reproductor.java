de87690d7514a6f181d23e19dc4415e51aa75994
N00284285-Alexandro-Arana <129539347+N00284285-Alexandro-Arana@users.noreply.github.com>
2023-05-28 02:43:02 -0500

    Primer Commit

---
+++ src/entidades/Reproductor.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+/**
+ *
+ * @author PierooA.S
+ */
+public interface Reproductor {
+    
+    public void reproducir();
+    public void adelantar();
+    public void retroceder();
+    
+}
